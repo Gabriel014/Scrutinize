def5cc9b58f71c33b8337627d410f6a212a87764
BrazilGabriel <gabriel.br1994@gmail.com>
2017-03-22 01:30:42 -0400

    Armazenamento dos gatos escolhidos

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -4,38 +4,47 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour {
-	bool pressed = false;
+	public List<int> index;
+	public List<string> selectedCats;
+	public Image cat0,cat1,cat2;
+	public Sprite imgCat0, imgCat1, imgCat2;
+	bool isOnList;
 
 
-	public void Pressed(int card)
+	public void Pressed(string card) //string contém 4 numeros sendo força, agilidade, fofura i ID respectivamente.
 	{
-		if (!pressed)
+		isOnList = false;
+		for(int i = 0; i < 3; i++)
 		{
-			
-			CharSelector.contPlayer+=1;
-			pressed = true;
-			gameObject.GetComponent<Image>().color = new Color32 (71,71,71,255);
-			PlayerPrefs.SetInt(("cat"+card.ToString()),1);
+			if(index[i] == int.Parse(card.Substring(3)))//procura se o card já está na lista
+			{
+				isOnList = true;//indica que o carda ja está na lista
+				index[i] = 0; // se ele já está na lista e foi clicado novamene, significa que foi desmarcado. Logo, tem que ser removido;
+				selectedCats[i] = "";
+				break; //finaliza o evento
+			}
 		}
-		else 
+		if(!isOnList)//se o card nao está na lista
 		{
-			CharSelector.contPlayer-=1;
-			pressed = false;
-			gameObject.GetComponent<Image>().color = new Color32 (255,255,255,255);
-			PlayerPrefs.SetInt(("cat"+card.ToString()),0);
-
+			for(int i = 0; i < 3; i++) 
+			{
+				if(index[i]==0)//procura-se um espaço vazio
+				{
+					index[i] = int.Parse(card.Substring(3));// e adiciona-se este card na lista
+					selectedCats[i] = card.Substring(0,4);
+					print(i+" index "+selectedCats[i]);
+					break;
+				}
+			}
 		}
 	}
-	void Update()
+
+
+	public void ConfirmSelection()
 	{
-		if(CharSelector.contPlayer==3&&!pressed)
-		{
-			gameObject.GetComponent<Button>().interactable = false;
-		}
-		else
-		{
-			gameObject.GetComponent<Button>().interactable = true;
-		}
+		print("cat1 "+selectedCats[0]);
+		print("cat2 "+selectedCats[1]);
+		print("cat3 "+selectedCats[2]);
 
 	}
 
---
+++ Assets/Scripts/ButtonStatus.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonStatus : MonoBehaviour {
+	bool pressed = false;
+
+	public void Pressed() //string contém 4 numeros sendo força, agilidade, fofura i ID respectivamente.
+	{
+		if (!pressed)
+		{
+			
+			CharSelector.contPlayer+=1;
+			pressed = true;
+			gameObject.GetComponent<Image>().color = new Color32 (71,71,71,255);
+		}
+		else 
+		{
+			CharSelector.contPlayer-=1;
+			pressed = false;
+			gameObject.GetComponent<Image>().color = new Color32 (255,255,255,255);
+
+		}
+	}
+	void Update()
+	{
+		if(CharSelector.contPlayer==3&&!pressed)GetComponent<Button>().interactable = false;
+		else GetComponent<Button>().interactable = true;
+	}
+}
