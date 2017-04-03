ff532f0db222cdb05be244dd12723a3b285ccd2e
BrazilGabriel <gabriel.br1994@gmail.com>
2017-04-03 13:53:20 -0400

    limpeza do código

--- Assets/Scripts/RewardsController.cs
+++ Assets/Scripts/RewardsController.cs
@@ -6,12 +6,10 @@ public class RewardsController : MonoBehaviour {
 	string type;
 	public float speed;
 	bool move=false;
-	int status=0;
 	public Transform initialPosition;
 	public void ShowCard(string cardType)
 	{
 		//tipo de carta posto no prefab.
-		status=1;
 		move=true;
 		type=cardType;
 		initialPosition=gameObject.GetComponent<Transform>();
@@ -20,18 +18,13 @@ public class RewardsController : MonoBehaviour {
 
 	void Update()
 	{
-		if(move&&status==1)
+		if(move)
 		{
 			float step = speed+Time.deltaTime;
 			transform.position=Vector3.MoveTowards(initialPosition.position,new Vector3(0,0,0),step);
 		}
-		else if(move&&status==-1)
-		{
-			float step = speed+Time.deltaTime;
-			transform.position=Vector3.MoveTowards(transform.position,initialPosition.position,step);
 
-		}
-		if((transform.position==new Vector3(0,0,0))&&status==1){
+		if(transform.position==new Vector3(0,0,0)){
 			gameObject.GetComponent<Animator>().enabled=true;
 			gameObject.GetComponent<Animator>().Play("bossAnimation");
 			
@@ -56,11 +49,11 @@ public class RewardsController : MonoBehaviour {
 		{
 			yield return new WaitForSeconds(2);
 			GameObject.Find("Test Button").GetComponent<TestManager>().BattleManager(gameObject);//aciona o script do TestManager e coloca como referencia esse GameObject
-			move=false;		}
+			move=false;
+		}
 
 	}
 	public void TurnBack(){
-		status=-1;
 		move=true;
 	}
 }
