22e7cd6cb258216c0e698c3c272eda4bbc62d090
BrazilGabriel <gabriel.br1994@gmail.com>
2017-04-07 00:03:20 -0400

    HUD de tesouro
    adicionado script de controle e animação

---
+++ Assets/Scripts/FoodController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FoodController : MonoBehaviour {
+
+	public int prizesCount,requestedPrizes;
+	public Text prizesText;
+
+
+	// Use this for initialization
+	public void FindFood()
+	{
+		prizesCount+=1;
+		prizesText.text=prizesCount+"/"+requestedPrizes;
+	}
+}
--- Assets/Scripts/RewardsController.cs
+++ Assets/Scripts/RewardsController.cs
@@ -49,6 +49,18 @@ public class RewardsController : MonoBehaviour {
 	{
 		if(type=="food")
 		{
+			yield return new WaitUntil(()=>Vector3.SqrMagnitude(transform.position - new Vector3(0,0,0))<0.0001);
+			gameObject.GetComponent<Animator>().enabled=true;
+			//gameObject.GetComponent<Animator>().SetBool("Flip", true);
+			yield return new WaitUntil(()=>transform.eulerAngles.y>=89);
+			gameObject.GetComponent<Image>().sprite=cardImage;
+			yield return new WaitUntil(()=>transform.eulerAngles.y<=0.2);
+			gameObject.GetComponent<Animator>().SetBool("ZoomCard",true);
+			yield return new WaitUntil(()=>transform.localScale.y >=15f);
+			gameObject.GetComponent<Animator>().SetBool("GoToPrize", true);
+			yield return new WaitUntil(()=>transform.localScale.y <=0.1f);
+			gameObject.SetActive(false);
+			GameObject.Find("PrizesHUD").GetComponent<FoodController>().FindFood();
 		}
 		if(type=="item")
 		{
